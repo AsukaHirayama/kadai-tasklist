a6b5ea4d4750c9b0f17715b7c8c5628ad94cdbf1
asuka.hirayama <asuka.hirayama@rakuten.com>
2018-05-28 03:51:12 +0000

    add status

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,19 +2,29 @@
 
 @section('content')
 
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
+    
+    @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+     {!! Form::label('status', 'status:') !!}
+        {!! Form::text('status') !!}
 
-        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::label('content', 'task:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
     
-    {{ '<p style="color: red;">When passed through htmlentities function</p>' }}
-    {!! '<p style="color: red;">When not passed through htmlentities function</p>' !!}
+
 
 @endsection
 
